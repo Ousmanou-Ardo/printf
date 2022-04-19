484a73ebca4a03b3b553ecf2183f1feb6c2960b0
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-04-19 14:08:25 -0400

    modified

--- _printf.c
+++ _printf.c
@@ -1,8 +1,20 @@
 #include "main.h"
 
-int run_printf(const char *format, va_list args);
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
 int _printf(const char *format, ...);
 
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
 
 /**
  * run_printf - Reads through the format string for _printf.
@@ -12,12 +24,13 @@ int _printf(const char *format, ...);
  *
  * Return: The number of characters stored to output.
  */
-int run_printf(const char *format, va_list args)
+int run_printf(const char *format, va_list args, buffer_t *output)
 {
 	int i, wid, prec, ret = 0;
 	char tmp;
 	unsigned char flags, len;
-	unsigned int (*f)(va_list, unsigned char);
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
 	for (i = 0; *(format + i); i++)
 	{
@@ -25,17 +38,17 @@ int run_printf(const char *format, va_list args)
 		if (*(format + i) == '%')
 		{
 			tmp = 0;
-		//	flags = handle_flags(format + i + 1, &tmp);
-		//	wid = handle_width(args, format + i + tmp + 1, &tmp);
-		//	prec = handle_precision(args, format + i + tmp + 1,
-		//			&tmp);
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
 			len = handle_length(format + i + tmp + 1, &tmp);
 
 			f = handle_specifiers(format + i + tmp + 1);
 			if (f != NULL)
 			{
 				i += tmp + 1;
-				ret += f(args, len);
+				ret += f(args, output, flags, wid, prec, len);
 				continue;
 			}
 			else if (*(format + i + tmp + 1) == '\0')
@@ -44,9 +57,10 @@ int run_printf(const char *format, va_list args)
 				break;
 			}
 		}
-		ret += _memcpy((format + i), 1);
+		ret += _memcpy(output, (format + i), 1);
 		i += (len != 0) ? 1 : 0;
 	}
+	cleanup(args, output);
 	return (ret);
 }
 
@@ -58,15 +72,19 @@ int run_printf(const char *format, va_list args)
  */
 int _printf(const char *format, ...)
 {
+	buffer_t *output;
 	va_list args;
 	int ret;
 
 	if (format == NULL)
 		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
 	va_start(args, format);
 
-	ret = run_printf(format, args);
+	ret = run_printf(format, args, output);
 
 	return (ret);
 }
